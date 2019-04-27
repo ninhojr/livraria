1de7e146a8c44da886febbc22a653b9f56e36b96
Janilson Junior <janilsonjrdias@gmail.com>
2019-04-27 11:54:20 -0300

    Corrigindo erro de sintaxe

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-__dirname() = path.resolve();
+__dirname = path.resolve();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false}));
